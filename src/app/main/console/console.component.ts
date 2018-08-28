597fe4f491372a198682e476ee6dba073136585e
Wolodya <vovatarasov6991@gmail.com>
2018-08-28 13:01:23 +0300

    add logs

--- src/app/main/console/console.component.ts
+++ src/app/main/console/console.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { map, repeatWhen, retryWhen, takeWhile, delay, take } from 'rxjs/operators';
+import { map, repeatWhen, retryWhen, takeWhile, delay, take, expand } from 'rxjs/operators';
 import { Observable, interval } from 'rxjs';
 import { RtmlsService } from '../../rtmls/rtmls.service';
 declare const $: any;
@@ -12,18 +12,26 @@ declare const $: any;
 })
 export class ConsoleComponent implements OnInit {
   private _alive = true;
+  private date: string;
   constructor(private rtmls: RtmlsService) {
 
   }
 
   ngOnInit() {
-    // this.rtmls.readLogs('2018-08-18 12:12:34', 'test_id'). pipe(
-     
-    //   repeatWhen(() => interval(1000)),      
-    //   takeWhile(() => this._alive)
-    // ).subscribe(res => {
-    //   console.log(res);
-    // });
+    this.rtmls.currentflightId.pipe(takeWhile(() => this._alive)).subscribe(res => {
+      this.date = this.getFormatedDate();
+      this.rtmls.readLogs(this.date, res).pipe(
+        expand(ex => {
+          this.date = this.getFormatedDate();
+          return this.rtmls.readLogs(this.date, res).pipe(delay(1000));
+        }
+        )
+      ).subscribe(res => {
+        console.log(res);
+      });
+
+    });
+
 
   }
 
@@ -36,4 +44,8 @@ export class ConsoleComponent implements OnInit {
     this._alive = false;
   }
 
+
+  getFormatedDate(): string {
+    return `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()} ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+  }
 }
