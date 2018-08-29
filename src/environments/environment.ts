afa33f44f8a0ea0c7f354184bb9f76a2d83d8ccc
bogdan-molodets <bogdan.molodets@gmail.com>
2018-08-29 10:23:23 +0300

    change api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   mapbox: {
     accessToken: 'pk.eyJ1IjoiYm9nZGFubW9sb2RldHMiLCJhIjoiY2pjMG9kZ3NjMDNhazJ4cXltNWdhYXh0diJ9.RbZ5rCF0N3-n5GKfGyrI3w'
   },
-  apiUrl:'http://192.168.92.33:5000/api/'
+  apiUrl:'http://192.168.0.200:5000/api/'
 };
 
 /*
