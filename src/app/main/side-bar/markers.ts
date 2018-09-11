180cef8692a234e7f6c1b2e0044de2df627a18d0
Wolodya <vovatarasov6991@gmail.com>
2018-09-11 17:25:00 +0300

    add markers fixture

---
+++ src/app/main/side-bar/markers.ts
@@ -0,0 +1,54 @@
+export const markers={
+    "marker": [
+      {
+        "azimuth": 0,
+        "distance": 0,
+        "llh": {
+          "hgt": 0,
+          "lat": 48.437425,
+          "lon": 35.035179
+        },
+        "marker_id": "1",
+        "marker_state": "super",
+        "timestamp": "string"
+      },
+      {
+        "azimuth": 0,
+        "distance": 0,
+        "llh": {
+          "hgt": 0,
+          "lat": 48.437546,
+          "lon": 35.035296
+        },
+        "marker_id": "2",
+        "marker_state": "super",
+        "timestamp": "string"
+      },
+      {
+        "azimuth": 0,
+        "distance": 0,
+        "llh": {
+          "hgt": 0,
+          "lat": 48.437401,
+          "lon": 35.035111
+        },
+        "marker_id": "3",
+        "marker_state": "super",
+        "timestamp": "string"
+      },
+      {
+        "azimuth": 0,
+        "distance": 0,
+        "llh": {
+          "hgt": 0,
+          "lat": 48.437655,
+          "lon": 35.035325
+        },
+        "marker_id": "4",
+        "marker_state": "super",
+        "timestamp": "string"
+      },
+
+    ],
+    "status": "Ok"
+  }
